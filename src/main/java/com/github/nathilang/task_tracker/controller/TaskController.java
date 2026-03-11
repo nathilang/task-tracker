dacac948f55764895d0e193854185d74e7f56d7c
nathilang <ilangnathaniel@gmail.com>
2026-03-11 10:23:22 +0800

    Initial Spring boot Task Tracker API

---
+++ src/main/java/com/github/nathilang/task_tracker/controller/TaskController.java
@@ -0,0 +1,33 @@
+package com.github.nathilang.task_tracker.controller;
+
+import com.github.nathilang.task_tracker.model.Task;
+import com.github.nathilang.task_tracker.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return service.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return service.getAllTasks();
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return service.updateTask(id, task);
+    }
+}
---
+++ src/main/java/com/github/nathilang/task_tracker/service/TaskService.java
@@ -0,0 +1,36 @@
+package com.github.nathilang.task_tracker.service;
+
+import com.github.nathilang.task_tracker.model.Task;
+import com.github.nathilang.task_tracker.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public Task createTask(Task task) {
+        return repository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        return repository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setCompleted(updatedTask.isCompleted());
+                    return repository.save(task);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+}
